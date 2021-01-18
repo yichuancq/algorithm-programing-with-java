1dfece2360e43a57fd227e1e29d854d28f586e17
yichuan <102027293@qq.com>
2021-01-18 19:51:41 +0800

    添加线性表操作

---
+++ src/main/java/com/example/learn/char3/Person.java
@@ -0,0 +1,39 @@
+package com.example.learn.char3;
+
+public class Person {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    Person() {
+    }
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
