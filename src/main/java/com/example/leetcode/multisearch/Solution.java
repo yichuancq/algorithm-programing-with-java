8c82e8e3c5bd9f77f2635d044e0914bcb0beff7f
yichuan <1012027293@qq.com>
2021-07-22 12:53:00 +0800

    添加多次搜索，单词替换算法

---
+++ src/main/java/com/example/leetcode/multisearch/Solution.java
@@ -0,0 +1,22 @@
+package com.example.leetcode.multisearch;
+
+/**
+ * 给定一个较长字符串big和一个包含较短字符串的数组smalls，设计一个方法，根据smalls中的每一个较短字符串，
+ * 对big进行搜索。输出smalls中的字符串在big里出现的所有位置positions,其中positions[i]为smalls[i]出现的所有位置
+ * 输入：
+ * big = "mississippi"
+ * smalls = ["is","ppi","hi","sis","i","ssippi"]
+ * 输出： [[1,4],[8],[],[3],[1,4,7,10],[5]]
+ */
+public class Solution {
+    /**
+     * 多次搜索
+     *
+     * @param big
+     * @param smalls
+     * @return
+     */
+    public int[][] multiSearch(String big, String[] smalls) {
+        return null;
+    }
+}
---
+++ src/main/java/com/example/leetcode/replacewords/Solution.java
@@ -0,0 +1,38 @@
+package com.example.leetcode.replacewords;
+
+import java.util.List;
+
+/**
+ * 在英语中，我们有一个叫做词根(root)的概念，它可以跟着其他一些词组成另一个较长的单词——我们称这个词为继承词(successor)。
+ * 例如，词根an，跟随着单词other(其他)，可以形成新的单词another(另一个)。
+ * <p>
+ * 现在，给定一个由许多词根组成的词典和一个句子。你需要将句子中的所有继承词用词根替换掉。如果继承词有许多可以形成它的词根，则用最短的词根替换它。
+ * <p>
+ * 示例 1：
+ * 输入：dictionary = ["cat","bat","rat"], sentence = "the cattle was rattled by the battery"
+ * 输出："the cat was rat by the bat"
+ * <p>
+ * 示例 2：
+ * 输入：dictionary = ["a","b","c"], sentence = "aadsfasf absbs bbab cadsfafs"
+ * 输出："a a b c"
+ * <p>
+ * 示例 3：
+ * 输入：dictionary = ["catt","cat","bat","rat"], sentence = "the cattle was rattled by the battery"
+ * 输出："the cat was rat by the bat"
+ */
+public class Solution {
+    /**
+     * 单词替换
+     *
+     * @param dictionary
+     * @param sentence
+     * @return
+     */
+    public String replaceWords(List<String> dictionary, String sentence) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
