b38fe3d1911b53e140b9d9d3c13c615c69c10c0a
yichuan <1012027293@qq.com>
2021-11-15 23:16:50 +0800

    添加观察者模式

--- src/main/java/com/example/designpattern/observer/ObserverPatternDemo.java
+++ src/main/java/com/example/designpattern/observer/ObserverPatternDemo.java
@@ -1,5 +1,13 @@
 package com.example.designpattern.observer;
 
+/**
+ * 使用场景：
+ * <p>
+ * 一个抽象模型有两个方面，其中一个方面依赖于另一个方面。将这些方面封装在独立的对象中使它们可以各自独立地改变和复用。
+ * 一个对象的改变将导致其他一个或多个对象也发生改变，而不知道具体有多少对象将发生改变，可以降低对象之间的耦合度。
+ * 一个对象必须通知其他对象，而并不知道这些对象是谁。
+ * 需要在系统中创建一个触发链，A对象的行为将影响B对象，B对象的行为将影响C对象……，可以使用观察者模式创建一种链式触发机制。
+ */
 public class ObserverPatternDemo {
     public static void main(String[] args) {
         Subject subject = new Subject();
