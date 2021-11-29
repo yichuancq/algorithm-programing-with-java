ed2320309bccb0b5076d2a4dde31199e93a5b6bd
yichuan <1012027293@qq.com>
2021-11-29 09:52:44 +0800

    给定一个非负整数x ,判断num是一个完全平方数

---
+++ src/main/java/com/example/leetcode/isperfectsquare/Solution.java
@@ -0,0 +1,50 @@
+package com.example.leetcode.isperfectsquare;
+
+/**
+ * 定一个正整数num，编写一个函数，如果num是一个完全平方数，则返回 true，否则返回false
+ * <p>
+ * 示例 1：
+ * 输入：num = 16
+ * 输出：true
+ * <p>
+ * 示例 2：
+ * 输入：num = 14
+ * 输出：false
+ */
+public class Solution {
+
+    /**
+     * 判断num是一个完全平方数
+     *
+     * @param num
+     * @return
+     */
+    public boolean isPerfectSquare(int num) {
+        boolean flag = false;
+        if (num == 0 || num == 1) {
+            return true;
+        }
+        int left = 1;
+        int right = num;
+        while (left < right) {
+            int middle = (right - left + 1) / 2 + left;
+            if ((long) (middle * middle) == num) {
+                flag = true;
+                return flag;
+            } else if ((long) middle * middle > num) {
+                right = middle - 1;
+            } else {
+                left = middle;
+            }
+        }
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int number = 17;
+        boolean flag = solution.isPerfectSquare(number);
+        System.out.println(flag);
+
+    }
+}
