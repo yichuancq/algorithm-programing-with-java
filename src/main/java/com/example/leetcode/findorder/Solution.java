21c8a8339417dee1725983ddf0e2552b75d5b288
yichuan <1012027293@qq.com>
2021-12-10 12:22:04 +0800

    图 课程表排序

---
+++ src/main/java/com/example/leetcode/findorder/Solution.java
@@ -0,0 +1,100 @@
+package com.example.leetcode.findorder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 现在你总共有 numCourses 门课需要选，记为0到numCourses - 1。给你一个数组prerequisites ，其中 prerequisites[i] = [ai, bi] ，
+ * 表示在选修课程 ai 前 必须 先选修bi 。
+ * 例如，想要学习课程 0 ，你需要先完成课程1 ，我们用一个匹配来表示：[0,1] 。
+ * 返回你为了学完所有课程所安排的学习顺序。可能会有多个正确的顺序，你只要返回 任意一种 就可以了。如果不可能完成所有课程，返回 一个空数组 。
+ * <p>
+ * 输入：numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+ * 输出：[0,2,1,3]
+ * 解释：总共有 4 门课程。要学习课程 3，你应该先完成课程 1 和课程 2。并且课程 1 和课程 2 都应该排在课程 0 之后。
+ * 因此，一个正确的课程顺序是[0,1,2,3] 。另一个正确的排序是[0,2,1,3] 。
+ */
+public class Solution {
+
+    // 存储有向图
+    List<List<Integer>> edges;
+    // 标记每个节点的状态：0=未搜索，1=搜索中，2=已完成
+    int[] visited;
+    // 用数组来模拟栈，下标 n-1 为栈底，0 为栈顶
+    int[] result;
+    // 判断有向图中是否有环
+    boolean valid = true;
+    // 栈下标
+    int index;
+
+    /**
+     * 课程查找
+     *
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        edges = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numCourses; ++i) {
+            edges.add(new ArrayList<Integer>());
+        }
+        visited = new int[numCourses];
+        result = new int[numCourses];
+        index = numCourses - 1;
+        for (int[] info : prerequisites) {
+            edges.get(info[1]).add(info[0]);
+        }
+        // 每次挑选一个「未搜索」的节点，开始进行深度优先搜索
+        for (int i = 0; i < numCourses && valid; ++i) {
+            if (visited[i] == 0) {
+                dfs(i);
+            }
+        }
+        if (!valid) {
+            return new int[0];
+        }
+        // 如果没有环，那么就有拓扑排序
+        return result;
+    }
+
+    /**
+     * 递归调用
+     *
+     * @param u
+     */
+    public void dfs(int u) {
+        // 将节点标记为「搜索中」
+        visited[u] = 1;
+        // 搜索其相邻节点
+        // 只要发现有环，立刻停止搜索
+        for (int v : edges.get(u)) {
+            // 如果「未搜索」那么搜索相邻节点
+            if (visited[v] == 0) {
+                dfs(v);
+                if (!valid) {
+                    return;
+                }
+            }
+            // 如果「搜索中」说明找到了环
+            else if (visited[v] == 1) {
+                valid = false;
+                return;
+            }
+        }
+        // 将节点标记为「已完成」
+        visited[u] = 2;
+        // 将节点入栈
+        result[index--] = u;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int numCourses = 4;
+        int[][] prerequisites = {{1, 0}, {2, 0}, {3, 1}, {3, 2}};
+        int result[] = solution.findOrder(numCourses, prerequisites);
+        System.out.println(Arrays.toString(result));
+    }
+
+}
