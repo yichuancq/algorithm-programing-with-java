e804e2e5d659dcca5565b05f7ad789f8f39a884e
yichuan <1012027293@qq.com>
2021-12-10 14:52:16 +0800

    设计 字典树

---
+++ src/main/java/com/example/leetcode/triedirc/Trie.java
@@ -0,0 +1,63 @@
+package com.example.leetcode.triedirc;
+
+/**
+ * 字典树
+ */
+public class Trie {
+    private Trie[] children;
+    private boolean isEnd;
+
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
+    }
+
+    /**
+     * @param word
+     */
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Trie node = searchPrefix(word);
+        return node != null && node.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        return searchPrefix(prefix) != null;
+    }
+
+    private Trie searchPrefix(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
+    }
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        System.out.println(trie.search("apple"));
+        trie.search("app");     // 返回 False
+        trie.startsWith("app"); // 返回 True
+        trie.insert("app");
+        trie.search("app");     // 返回 True
+
+    }
+}
