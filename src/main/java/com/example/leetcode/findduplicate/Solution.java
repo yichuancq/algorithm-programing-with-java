6414a7fba613e606f15f1bc846afe9a0e6a6783f
yichuan <1012027293@qq.com>
2021-12-12 20:27:58 +0800

    二分搜索 查找重复的数

---
+++ src/main/java/com/example/leetcode/findduplicate/Solution.java
@@ -0,0 +1,42 @@
+package com.example.leetcode.findduplicate;
+
+/**
+ * 给定一个包含n + 1 个整数的数组nums ，其数字都在 1 到 n之间（包括 1 和 n），可知至少存在一个重复的整数。
+ * 假设 nums 只有 一个重复的整数 ，找出 这个重复的数 。
+ * 你设计的解决方案必须不修改数组 nums 且只用常量级 O(1) 的额外空间。
+ * <p>
+ * 示例 1：
+ * 输入：nums = [1,3,4,2,2]
+ * 输出：2
+ */
+public class Solution {
+
+    public int findDuplicate(int[] nums) {
+        /**
+         快慢指针思想, fast 和 slow 是指针, nums[slow] 表示取指针对应的元素
+         注意 nums 数组中的数字都是在 1 到 n 之间的(在数组中进行游走不会越界),
+         因为有重复数字的出现, 所以这个游走必然是成环的, 环的入口就是重复的元素,
+         即按照寻找链表环入口的思路来做
+         **/
+        int fast = 0, slow = 0;
+        while (true) {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+            if (slow == fast) {
+                fast = 0;
+                while (nums[slow] != nums[fast]) {
+                    fast = nums[fast];
+                    slow = nums[slow];
+                }
+                return nums[slow];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 3, 4, 2, 2};
+        int n = solution.findDuplicate(nums);
+        System.out.println(n);
+    }
+}
