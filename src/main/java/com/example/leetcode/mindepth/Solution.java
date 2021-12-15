3d645f0489bb464dbea036ec4fc6f7e9f51071f1
yichuan <1012027293@qq.com>
2021-12-15 20:05:08 +0800

    递归 二叉树的最小深度

---
+++ src/main/java/com/example/leetcode/mindepth/Solution.java
@@ -0,0 +1,37 @@
+package com.example.leetcode.mindepth;
+
+import com.example.leetcode.node.TreeNode;
+
+/**
+ * 二叉树的最小深度
+ * 给定一个二叉树，找出其最小深度。
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ * 说明：叶子节点是指没有子节点的节点。
+ */
+public class Solution {
+    /**
+     * 最小深度
+     *
+     * @param root
+     * @return
+     */
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int minDepth = Integer.MAX_VALUE;
+        if (root.left != null) {
+            minDepth = Math.min(minDepth(root.left), minDepth);
+        }
+        if (root.right != null) {
+            minDepth = Math.min(minDepth(root.right), minDepth);
+        }
+
+        return minDepth + 1;
+    }
+
+
+}
