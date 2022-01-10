9fad3056a8e0588d8590fe2eb7164817a5bf4f65
yichuan <1012027293@qq.com>
2022-01-11 06:42:32 +0800

    CountDownLatch

---
+++ src/main/java/com/example/examination/countdownlatch/CountDownLatchTest.java
@@ -0,0 +1,64 @@
+package com.example.examination.countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * CountDownLatch 的作用就是 允许 count 个线程阻塞在一个地方，直至所有线程的任务都执行完毕
+ *
+ * @author yichuan
+ */
+public class CountDownLatchTest {
+
+    /**
+     * 主线程开始执行…… ……
+     * 等待两个线程执行完毕…… ……
+     * 子线程：pool-1-thread-1执行
+     * 子线程：pool-2-thread-1执行
+     * 两个子线程都执行完毕，继续执行主线程
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        final CountDownLatch latch = new CountDownLatch(2);
+        System.out.println("主线程开始执行…… ……");
+        //第一个子线程执行
+        ExecutorService es1 = Executors.newSingleThreadExecutor();
+        es1.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                    System.out.println("子线程：" + Thread.currentThread().getName() + "执行");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                latch.countDown();
+            }
+        });
+        es1.shutdown();
+        //第二个子线程执行
+        ExecutorService es2 = Executors.newSingleThreadExecutor();
+        es2.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("子线程：" + Thread.currentThread().getName() + "执行");
+                latch.countDown();
+            }
+        });
+        es2.shutdown();
+        System.out.println("等待两个线程执行完毕…… ……");
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("两个子线程都执行完毕，继续执行主线程");
+    }
+}
