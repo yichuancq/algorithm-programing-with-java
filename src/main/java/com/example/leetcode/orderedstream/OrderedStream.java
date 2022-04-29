292999831bbf88f3bea81d81ac4a08236c11f241
yichuan <1012027293@qq.com>
2022-04-29 21:48:12 +0800

    fix code

---
+++ src/main/java/com/example/leetcode/orderedstream/OrderedStream.java
@@ -0,0 +1,22 @@
+package com.example.leetcode.orderedstream;
+
+import java.util.List;
+
+/**
+ * 设计有序流
+ */
+public class OrderedStream {
+    public OrderedStream(int n) {
+
+    }
+
+    public List<String> insert(int idKey, String value) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        //todo 设计有序流
+    }
+
+}
